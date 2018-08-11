d546c2ab2374750d270bb0cb6545b18ef30c0465
programmerlab <kroy.iips@gmail.com>
2018-08-12 04:11:50 +0530

    mail payment gatwaty

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -5,8 +5,137 @@
   @stop 
   @section('content') 
       <!-- Left side column. contains the logo and sidebar --> 
-   @include('layouts.home-slider') 
+ <div class="intro-area banner-section">
+           <div class="main-overly"></div>
+            <div class="intro-carousel">
+                <div class="intro-content">
+                    <div class="slider-images">
+                        <img src="{{ asset('img/slider/s1.jpg') }}" alt="">
+                    </div>
+                    <div class="slider-content">
+                        <div class="display-table">
+                            <div class="display-table-cell">
+                                <div class="container"> 
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            
+                                            <div class="layer-1-2" style="margin-top: 0px;">
+                                                <h2 class="title22" style="color: #fff">World's Larest Market Research Reports Portal</h2>
+                                            </div> 
+                                            <div class="layer-1-1 ">
+                                                <p>Search from over 500,000 In-Depth Market Research Reports & Free Sample Reports!</p> 
+                                            </div>
 
+                                    <div class="input-group md-form form-sm form-2 pl-0">
+                                        <form action="{{url('research-reports')}}">
+                                        <input class="form-control my-0 py-1 amber-border" type="text" name="search" placeholder="Search Your Keywords..." aria-label="Search">
+                                        <div class="input-group-append"> 
+                                          <button type="submit" class="single_add_to_cart_button btn btn-danger alt">Search</button>
+                                        </div>
+                                        </form>
+
+                                    </div>
+
+
+                                                                                </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="intro-content">
+                    <div class="slider-images">
+                        <img src="{{ asset('img/slider/s1.jpg') }}" alt="">
+                    </div>
+                    <div class="slider-content">
+                        <div class="display-table">
+                            <div class="display-table-cell">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            
+                                            <div class="layer-1-2" style="margin-top: 0px;">
+                                                <h2 class="title22" style="color: #fff">World's Larest Market Research Reports Portal</h2>
+                                            </div> 
+                                            <div class="layer-1-1 ">
+                                                <p>Search from over 500,000 In-Depth Market Research Reports & Free Sample Reports!</p> 
+                                            </div>
+
+                                    <div class="input-group md-form form-sm form-2 pl-0">
+                                        <form action="{{url('research-reports')}}">
+                                        <input class="form-control my-0 py-1 amber-border" type="text" name="search" placeholder="Search Your Keywords..." aria-label="Search">
+                                        <div class="input-group-append"> 
+                                          <button type="submit" class="single_add_to_cart_button btn btn-danger alt">Search</button>
+                                        </div>
+                                        </form>
+
+                                    </div>
+
+
+                                                                                </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="inner-main">
+                    
+                        <div class="col-md-12 col-sm-12 col-xs-12 services-column">
+                            <div class="row">
+                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                    <div class="well-services">
+                                        <div class="well-icon">
+                                            <a href="#"><i class="fa fa-umbrella" ></i></a>                                     </div>
+                                        <div class="well-content">
+                                            <h4><a href="#">Quality <br/>Assurence</a></h4>
+                                            <p>Ensure your fingertips with our high standards database of market research.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- single-well end-->
+                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                    <div class="well-services ">
+                                        <div class="well-icon">
+                                            <a href="#"><i class="fa fa-clock-o" ></i></a>                                      </div>
+                                        <div class="well-content">
+                                            <h4><a href="#">24X7 Customer <br/>Service</a></h4>
+                                            <p>Assistance at your finger tips.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- single-well end-->
+                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                    <div class="well-services">
+                                        <div class="well-icon">
+                                            <a href="#"><i class="fa fa-certificate" ></i></a>                                      </div>
+                                        <div class="well-content">
+                                            <h4><a href="#">Customization </a></h4>
+                                            <p>We provide research according to customer requirements.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- single-well end-->
+                                <div class="col-md-3 hidden-sm col-xs-12">
+                                    <div class="well-services">
+                                        <div class="well-icon">
+                                            <a href="#"><i class="fa fa-lock" ></i></a>                                     </div>
+                                        <div class="well-content">
+                                            <h4><a href="#">Secured Payment <br/>Option</a></h4>
+                                            <p>Your trust, our security.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- single-well end-->
+                            </div>
+                        </div>
+                    </div>
+        </div>
+        <!-- End Slider Area -->
+        
         <!-- End Slider Area -->
     
     <div class="blog-area area-padding">
