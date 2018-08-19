d1a77567d1cf03ce19893d8bd229d435299939a6
programmerlab <kroy.iips@gmail.com>
2018-08-20 01:57:01 +0530

    paypal

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,7 +59,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {   
-        dd($e);
         $path_info_url = $request->getpathInfo();
           if(strpos($path_info_url,'admin')==false){
            return Redirect::to('404');
