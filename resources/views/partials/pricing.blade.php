d2edadca5629cc7d542492ff9cb4f2309f8ea64e
programmerlab <kroy.iips@gmail.com>
2018-08-22 04:02:39 +0530

    img

--- resources/views/partials/pricing.blade.php
+++ resources/views/partials/pricing.blade.php
@@ -2,14 +2,19 @@
             <h4>Price</h4>
 </div>
     <form action="{{url('payment')}}">
-    <div class="plan-pricing">
+    <div class="plan-pricing" style="color: #000 !important">
         <input type="hidden" name="payment_id" value="{{$data->id}}">
-        <p><input type="radio" name="price" value="{{$data->signle_user_license}}" checked=""> <span>
-            <b>Single User License </b>: US $ {{$data->signle_user_license}}      </span></p>
-        <p><input type="radio" name="price" value="{{$data->multi_user_license}}"> <span>
-            <b>Multi User License </b>: US $ {{$data->multi_user_license}}       </span></p>
-        <p><input type="radio" name="price" value="{{$data->corporate_user_license}}"> <span>
-            <b>Corporate User License </b>: US $ {{$data->corporate_user_license}} </span></p>
+        <div>
+            <input type="radio" name="price" value="{{$data->signle_user_license}}" checked="checked" style="display: block;"> 
+            <span>
+            <b>Single User License </b>: US $ {{$data->signle_user_license}}      </span>
+        </div>
+        <div><input type="radio" name="price" value="{{$data->multi_user_license}}" style="display: block;"> <span>
+            <b>Multi User License </b>: US $ {{$data->multi_user_license}}       </span>
+        </div>
+        <div><input type="radio" name="price" value="{{$data->corporate_user_license}}" style="display: block;"> <span>
+            <b>Corporate User License </b>: US $ {{$data->corporate_user_license}} </span>
+        </div>
         <button type="submit" class="btn btn-danger" style="font-size: 18px">
             <i class="fa fa-shopping-cart" aria-hidden="true"></i>
             </span> <b> Buy Now!</b>
